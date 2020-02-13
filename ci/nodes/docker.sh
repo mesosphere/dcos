09002e296ff609b2c8a5fae8f45a03996eae19a3
Karsten Jeschkies <k@jeschkies.xyz>
2020-02-13 16:11:38 +0100

    Startup Docker.

--- ci/nodes/docker.sh
+++ ci/nodes/docker.sh
@@ -5,3 +5,5 @@ set -e
 sudo yum install -y yum-utils device-mapper-persistent-data lvm2
 sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 sudo yum install -y docker-ce docker-ce-cli containerd.io
+sudo systemctl start docker
+sudo usermod -a -G docker $USER
