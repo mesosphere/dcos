a8014a9d5e16a47cfe43a4fb9530fb439c272eae
Tomasz Janiszewski <tjaniszewski@d2iq.com>
2020-07-10 17:04:14 +0200

    Fix missing agent

--- workloads/example-team-1/example-workload-A/workload-test.groovy
+++ workloads/example-team-1/example-workload-A/workload-test.groovy
@@ -1,9 +1,10 @@
 pipeline {
-    stages {
-        stage('build') {
-            steps {
-                sh 'docker run --rm -v $PWD:/usr/src -w /usr/src python:3.7 python --version'
-            }
-        }
+  agent any
+  stages {
+    stage('build') {
+      steps {
+        sh 'docker run --rm -v $PWD:/usr/src -w /usr/src python:3.7 python --version'
+      }
     }
+  }
 }
--- workloads/example-team-2/example-workload-B/workload-test.groovy
+++ workloads/example-team-2/example-workload-B/workload-test.groovy
@@ -1,9 +1,10 @@
 pipeline {
-    stages {
-        stage('build') {
-            steps {
-                sh 'docker run --rm -v $PWD:/usr/src -w /usr/src golang:latest go version'
-            }
-        }
+  agent any
+  stages {
+    stage('build') {
+      steps {
+        sh 'docker run --rm -v $PWD:/usr/src -w /usr/src golang:latest go version'
+      }
     }
+  }
 }
